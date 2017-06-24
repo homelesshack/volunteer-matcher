3add2b9d815905651b9273c959e0ea0fe7b29c79
Craig J. Bass <craig@madetech.co.uk>
2017-06-24 10:13:40 +0100

    Volunteer controller

---
+++ app/controllers/volunteer_controller.rb
@@ -0,0 +1,4 @@
+class VolunteerController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'volunteer/signup', to: 'volunteer#new'
+
   devise_for :accounts
   root to: "home#index"
 end
